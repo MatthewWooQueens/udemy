2e55c07b33a249fb5331a69c72e424c2ef1d62b2
Matthew Woo <20mw6@queensu.ca>
2024-05-25 13:21:33 -0400

    api and emails

--- api/main.py
+++ api/main.py
@@ -1,9 +1,13 @@
 import requests
+from send_email import send_email
 
 api_key = "a017f7d89ba24a81a42d155702ef08c6"
 url = "https://newsapi.org/v2/everything?q=tesla&from=2024-04-25&sortBy=publishedAt&apiKey=a017f7d89ba24a81a42d155702ef08c6"
 
 request = requests.get(url)
 content = request.json()
+msg = ""
 for article in content["articles"]:
-    print(article["title"])
+    msg = ''.join((msg, f'{article["title"]}\n{article["description"]}\n',))
+msg = u''.join(msg).encode('utf-8').strip()
+send_email(msg)
---
+++ api/send_email.py
@@ -0,0 +1,14 @@
+import smtplib, ssl
+
+def send_email(msg):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "rutyreal@gmail.com"
+    password = "vmcf tcvr mtmy eqwc"
+    reciever = "rutyreal@gmail.com"
+    context = ssl.create_default_context()
+    message = msg
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, reciever, message)
+        
